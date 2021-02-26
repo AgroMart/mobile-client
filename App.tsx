d5c6717478441c6f9c7806313b5d71fdba6f679d
MatheusEstanislau <matheus.estanislau7@gmail.com>
2021-02-26 18:55:55 -0300

    Adicionado componente BottomTabNavigation

--- App.tsx
+++ App.tsx
@@ -1,23 +1,8 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import BottomTabNavigation from './src/components/BottomTabNavigation/index';
 
-const App: React.FC = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Hello from AgroMart!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App: React.FunctionComponent = () => {
+  return <BottomTabNavigation />;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App;
