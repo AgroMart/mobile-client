20c677f99f726d1c810b6f0610c7bd315b7c50b6
João Soares <jpcirqueira81@gmail.com>
2021-03-02 18:29:55 -0300

    adicionado styles RadioButton.

---
+++ src/components/RadioButton/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components/native';
+
+export const ViewDisable = styled.View`
+  height: 24;
+  width: 24;
+  borderWidth: 2;
+  borderColor: #00CC76;
+  alignItems: center;
+  borderRadius: 20;
+  justifyContent: center;
+`;
+
+export const ViewEnable = styled.View`
+  height: 14;
+  width: 14;
+  borderRadius: 6;
+  backgroundColor: #00CC76;
+`;
+
