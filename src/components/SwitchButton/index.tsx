26508926e4198c198f94c5bbd07b1464f8585318
João Soares <jpcirqueira81@gmail.com>
2021-03-01 21:21:34 -0300

    adicionado SwitchButton.

---
+++ src/components/SwitchButton/index.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { View, Switch } from "react-native";
+
+const SwitchButton = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+  return (
+    <View >
+      <Switch
+        trackColor={{ false: "#BFC6C3", true: "#00AA95" }}
+        thumbColor={isEnabled ? "#00AA95" : "#00AA95"}
+        ios_backgroundColor="#BFC6C3"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
+    </View>
+  );
+}
+
+export default SwitchButton;
