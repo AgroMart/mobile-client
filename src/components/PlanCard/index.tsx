d3e6143c9e25a1640bd2a2d0f601e0225d5880e7
João Soares <jpcirqueira81@gmail.com>
2021-03-04 18:34:22 -0300

    adicionado PlanCard.

---
+++ src/components/PlanCard/index.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Card, Image, InfoTitle, Info, InfoView} from "./styles";
+import SwitchButton from '../SwitchButton';
+import RadioButton from '../RadioButton';
+
+interface StoreCardProps {
+  loja: string;
+  contato: string;
+  dataAquisicao: string;
+  cestasDisponiveis: string;
+  cestasRecebidas: string;
+  imagem: string;
+}
+
+const PlanCard: React.FC<StoreCardProps> = ({
+  imagem,
+  loja,
+  contato,
+  dataAquisicao,
+  cestasDisponiveis,
+  cestasRecebidas}) => {
+  return (
+      <Card>
+        <Image
+        source={{
+          uri: imagem,
+        }}/>
+        <InfoView>
+            <InfoTitle>Loja: </InfoTitle>
+            <Info>{loja}</Info>
+        </InfoView>
+        <InfoView>
+            <InfoTitle>Contato: </InfoTitle>
+            <Info>{contato}</Info>
+        </InfoView>
+        <InfoView>
+            <InfoTitle>Data de aquisição: </InfoTitle>
+            <Info>{dataAquisicao}</Info>
+        </InfoView>
+        <InfoView>
+            <InfoTitle>Cesta(s) disponíveis: </InfoTitle>
+            <Info>{cestasDisponiveis}</Info>
+        </InfoView>
+        <InfoView>
+            <InfoTitle>Cesta(s) recebidas: </InfoTitle>
+            <Info>{cestasRecebidas}</Info>
+        </InfoView>
+        <InfoView>
+            <InfoTitle>Pular cesta da semana </InfoTitle>
+            <SwitchButton />
+        </InfoView>
+        <InfoView>
+            <InfoTitle>Receber cesta  </InfoTitle>
+            <RadioButton />
+        </InfoView>
+        <InfoView>
+            <InfoTitle>Buscar  </InfoTitle>
+            <RadioButton />
+        </InfoView>
+      </Card>
+  );
+}
+
+export default PlanCard;
---
+++ src/components/PlanCard/styles.ts
@@ -0,0 +1,46 @@
+import styled from 'styled-components/native';
+//import FastImage from 'react-native-fast-image';
+
+export const Card = styled.View.attrs({
+  shadowOffset: { height: 0, width: 0 },
+  shadowOpacity: 0.22,
+  shadowRadius: 2.22,
+  elevation: 1,
+})`
+  width:84%;
+  height: 68%;
+  flex-direction: column;
+  border-bottom-width: 0px;
+  background-color: white;
+  margin-left: 8%;
+  margin-right: 8%;
+  padding: 8px;
+  border-radius: 4px;
+  border-width: 0px;
+  align-self: stretch;
+  margin-bottom: 14px;
+  margin-top: 20%;
+`;
+
+export const Image = styled.Image`
+align-self: stretch;
+height: 130px;
+width: 100%;
+margin-bottom: 8px;
+border-radius: 3px;
+`;
+
+
+export const Info = styled.Text`
+  font-size: 20px;
+`;
+export const InfoView = styled.View`
+  flex-direction: row;
+  margin-left: 10px;
+  margin-top: 2px;
+`;
+
+export const InfoTitle = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+`;
