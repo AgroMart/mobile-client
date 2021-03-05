a97ec93afb1d912f51e4e2f832a9d8e056756721
João Soares <jpcirqueira81@gmail.com>
2021-03-05 10:03:01 -0300

    ajuste switchButton.

--- src/components/SwitchButton/index.tsx
+++ src/components/SwitchButton/index.tsx
@@ -1,18 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import { View, Switch } from "react-native";
 
-const SwitchButton: React.FC = () => {
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+type SwitchButtonProps = {
+  value: boolean;
+  enable: () => void;
+};
 
+const SwitchButton: React.FC<SwitchButtonProps> = ({value, enable}) => {
   return (
     <View >
       <Switch
         trackColor={{ false: "#BFC6C3", true: "#00AA95" }}
-        thumbColor={isEnabled ? "#00AA95" : "#00AA95"}
+        thumbColor={value ? "#00AA95" : "#00AA95"}
         ios_backgroundColor="#BFC6C3"
-        onValueChange={toggleSwitch}
-        value={isEnabled}
+        onValueChange={enable}
+        value={value}
       />
     </View>
   );
