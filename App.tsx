bc53f60f6e0334bf27db1da6dd123d43e77defa5
byronkamal <byronkamal@hotmail.com>
2022-03-13 11:34:47 -0300

    #6 Iniciando funcionalidade de push notification

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 /* eslint-disable camelcase */
 import React, { useEffect } from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, Platform } from 'react-native';
 import AppLoading from 'expo-app-loading';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
 import { NavigationContainer } from '@react-navigation/native';
 import * as Updates from 'expo-updates';
 import {
@@ -28,8 +30,37 @@ const App: React.FC = () => {
   };
 
   useEffect(() => {
+    registerForPushNotificationsAsync().then(token => console.log(token)).catch(err => console.log(err));
     updateApp();
   }, []);
+
+  const registerForPushNotificationsAsync = async () => {
+    if (Device.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      const token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+    };
 
   const [fontsLoaded] = useFonts({
     Montserrat_400Regular,
