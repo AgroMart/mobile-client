0f051603991c4e07f2b190c1d9347ecd67dc0162
Christian Siqueira <chfleurysiq@gmail.com>
2024-08-07 19:39:49 -0300

    refactor: api dicionario name

--- src/services/api-dicionario.ts
+++ src/services/api-dicionario.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 export const baseURL = 'http://ec2-3-90-151-222.compute-1.amazonaws.com:3000/';
-const api = axios.create({
+const apiDicionario = axios.create({
   baseURL,
   timeout: 9000,
   headers: { Accept: 'application/json' },
 });
 
-export default api;
+export default apiDicionario;
