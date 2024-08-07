f6af4664138063f8bba23f9abeec052055464b4c
Christian Siqueira <chfleurysiq@gmail.com>
2024-08-07 18:57:08 -0300

    fix: baseUrl

--- src/services/api-dicionario.ts
+++ src/services/api-dicionario.ts
@@ -1,13 +1,10 @@
 import axios from 'axios';
 
-export const baseURL = process.env.NODE_ENV === 'development' ? 'https://z5a5lohzk73nhveu5wsb5youne0etjlf.lambda-url.sa-east-1.on.aws/' : `http://${process.env.LOCAL_IP}:3000/`;
-
+export const baseURL = 'http://ec2-3-90-151-222.compute-1.amazonaws.com:3000/';
 const api = axios.create({
   baseURL,
   timeout: 9000,
-  headers: {Accept: "application/json"
-  }
-
+  headers: { Accept: 'application/json' },
 });
 
 export default api;
