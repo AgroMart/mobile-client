4aa4fa28f7bbac2f16615c5dc6ddcc58d0a1ba7c
Christian Siqueira <chfleurysiq@gmail.com>
2024-08-11 18:20:34 -0300

    fix: map images properly with baseUrl

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -12,7 +12,50 @@ import Carousel from '../../components/Carousel';
 import StoreCard from '../../components/StoreCard';
 
 import { Container, CarouselContainer, StoresContainer } from './styles';
-import AsyncStorage from '@react-native-async-storage/async-storage'; 
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+function appendBaseUrlToImageUrls(data: any, baseUrl: string) {
+  data.forEach((loja: any) => {
+    if (loja?.cestas?.length) {
+      for (let cesta of loja.cestas) {
+        cesta.url = baseUrl + cesta?.imagem[0]?.url;
+      }
+    }
+
+    if (loja?.planos?.length) {
+      for (let plano of loja.planos) {
+        if (plano?.imagem?.length) {
+          plano.url = baseUrl + plano.imagem[0]?.url;
+        } else {
+          plano.url = baseUrl + plano?.imagem?.url;
+        }
+      }
+    }
+
+    if (loja?.produto_avulsos?.length) {
+      for (let produto of loja.produto_avulsos) {
+        if (produto?.imagem?.length) {
+          produto.url = baseUrl + produto.imagem[0]?.url;
+        } else {
+          produto.url = baseUrl + produto?.imagem?.url;
+        }
+      }
+    }
+
+    if (loja.banner && loja.banner.url) {
+      loja.banner.url = baseUrl + loja.banner.url;
+    }
+
+    if (loja.banner && loja.banner.formats) {
+      for (let format in loja.banner.formats) {
+        if (loja.banner.formats[format].url) {
+          loja.banner.formats[format].url =
+            baseUrl + loja.banner.formats[format].url;
+        }
+      }
+    }
+  });
+}
 
 const Home: React.FC = () => {
   const navigation = useNavigation();
@@ -25,19 +68,18 @@ const Home: React.FC = () => {
       const baseUrl = await AsyncStorage.getItem('@BaseUrlChosen');
 
       try {
-
         // Only try to load stores at home if Url has been chosen.
         // The api should be a singleton with an public attribute that tells if its Url is already defined
         // Do this in a future refactor
-        if(baseUrl?.length) {
+        if (baseUrl?.length) {
           const api = await initializeApi();
 
           const response = await api.get('lojas');
+          appendBaseUrlToImageUrls(response.data, baseUrl);
           console.log('response =====> ', response.data);
+
           updateStores(response.data);
         }
-
-
       } catch (err) {
         console.dir('AQUI ERRO LOJA' + err, { depth: null });
         console.log('ERROLOJA');
